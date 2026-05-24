c44590fc673e3cb118332b15300407f22050b21c
Preetinder Singh <singhpreetinder65@gmail.com>
2026-05-24 18:05:08 +0000

    .

--- sakila-sql-analytics.sql
+++ sakila-sql-analytics.sql
@@ -1,20 +1,20 @@
 
 USE sakila;
 
--- Question 1
+-- Q1
 SELECT c.first_name , c.last_name, r.rental_date
 FROM customer c
 JOIN rental r
 ON c.customer_id = r.customer_id;
 
--- Question 2
+-- Q2
 SELECT c.first_name, c.last_name, a.address
 FROM customer c
 JOIN address a
 ON c.address_id = a.address_id
 WHERE a.district IN ('Alberta', 'Texas');
 
--- Question 3
+-- Q3
 SELECT f.title, c.name AS category_name
 FROM film f
 LEFT JOIN film_category fc
@@ -23,7 +23,7 @@ LEFT JOIN category c
 ON fc.category_id = c.category_id
 ORDER BY c.name;
 
--- Question 4
+-- Q4
 SELECT s.store_id, COUNT(r.rental_id) AS total_rentals
 FROM rental r
 JOIN inventory i
@@ -32,7 +32,7 @@ JOIN store s
 ON i.store_id = s.store_id
 GROUP BY s.store_id;
 
--- Question 5
+-- Q5
 SELECT c.name, COUNT(fc.film_id) AS total_films
 FROM category c
 JOIN film_category fc
@@ -40,7 +40,7 @@ ON c.category_id = fc.category_id
 GROUP BY c.category_id
 HAVING COUNT(fc.film_id) > 50;
 
--- Question 6
+-- Q6
 SELECT f.title
 FROM film f
 JOIN inventory i
@@ -54,7 +54,7 @@ WHERE r.customer_id = (
     AND last_name = 'Smith'
 );
 
--- Question 7
+-- Q7
 SELECT f.title, SUM(p.amount) AS total_income
 FROM payment p
 JOIN rental r
@@ -65,7 +65,7 @@ JOIN film f
 ON i.film_id = f.film_id
 GROUP BY f.film_id;
 
--- Question 8
+-- Q8
 SELECT CONCAT(a.first_name, ' ', a.last_name) AS actor_name, COUNT(fa.film_id) AS total_films
 FROM actor a
 JOIN film_actor fa
@@ -73,7 +73,7 @@ ON a.actor_id = fa.actor_id
 GROUP BY a.actor_id
 ORDER BY total_films DESC;
 
--- Question 9
+-- Q9
 SELECT rental_id, customer_id,
 	CASE
 		WHEN customer_id > 300 THEN 'New Customer'
@@ -81,14 +81,14 @@ SELECT rental_id, customer_id,
         END AS customer_type
 FROM rental;
 
--- Question 10
+-- Q10
 SELECT DISTINCT f.title, f.release_year
 FROM film f
 JOIN inventory i
 ON f.film_id = i.film_id
 WHERE SUBSTRING(f.title, 1, 1) = 'A';
 
--- Question 11
+-- Q11
 CREATE DATABASE student_db;
 
 USE student_db;
